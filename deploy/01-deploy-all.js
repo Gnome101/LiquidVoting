4161b903a9566b6a8b11f5a2a23f33a5876161e2
Gnome101 <polygongnome@gmail.com>
2023-06-24 01:04:18 -0400

    added to deploy script

--- deploy/01-deploy-all.js
+++ deploy/01-deploy-all.js
@@ -6,8 +6,28 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   log("------------------------------------------------------------");
   //const decim = ethers.utils.parseEther("1");
   let args = [];
-
-  const timeLock = await deploy("TimeLock", {
+  //They deploy a governnace token first
+  const HOG = await deploy("IERC20", {
+    from: deployer,
+    args: args,
+    log: true,
+    blockConfirmations: 2,
+  });
+  console.log(HOG.address);
+  //They deploy the GSC next, I will skip this because I cannot find its code
+  // args = [];
+  // const governanceSteering = await deploy("governanceSteering", {
+  //   from: deployer,
+  //   args: args,
+  //   log: true,
+  //   blockConfirmations: 2,
+  // });
+  // console.log(governanceSteering.address);
+  log("------------------------------------------------------------");
+  const nonAddy = "0x0000000000000000000000000000000000000000"
+  args = [10,deployer.address,nonAddy];
+  //I am setting the GSC as 0
+  const timeLock = await deploy("Timelock", {
     from: deployer,
     args: args,
     log: true,
@@ -15,16 +35,8 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   });
   console.log(timeLock.address);
   log("------------------------------------------------------------");
-  args = [];
-  const governanceSteering = await deploy("governanceSteering", {
-    from: deployer,
-    args: args,
-    log: true,
-    blockConfirmations: 2,
-  });
-  console.log(governanceSteering.address);
-  log("------------------------------------------------------------");
-  const votingVault = await deploy("votingVault", {
+  args = [timeLock.address];
+  const votingVault = await deploy("Treasury", {
     from: deployer,
     args: args,
     log: true,
@@ -32,6 +44,7 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
   });
   console.log(votingVault.address);
   log("------------------------------------------------------------");
+  args = [timeLock.address,,10,10,];
 
   const coreVoting = await deploy("coreVoting", {
     from: deployer,
