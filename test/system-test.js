079c987e95ad587b73823be3c8ad85042eefc00b
Gnome101 <polygongnome@gmail.com>
2023-06-24 12:02:15 -0400

    added more v3 setup

--- test/system-test.js
+++ test/system-test.js
@@ -9,7 +9,7 @@ const {
   calculatePriceFromX96,
   getNearestUsableTick,
   getWalletEthBalance,
-} = require("../utils/TokenUtil");
+} = require("../utils/V3Tools");
 
 const bigDecimal = require("js-big-decimal");
 describe("Council Tests", function () {
@@ -68,19 +68,72 @@ describe("Council Tests", function () {
         const fee = 3000;
         const decimals = 18;
         //Price of one Hogwell in EPICDai
-        let price = 10;
-        let sqrtPrice;
+        let price = 1;
+        //let sqrtPrice;
 
         //Need to sort before hand of coruse
         let erc20Address = [mockWeth.address, mockHog.address];
         erc20Address = erc20Address.sort();
 
-        if (erc20Address[0] == HOGWELL.address) {
-          sqrtPrice = calculateSqrtPriceX96(price, decimals, decimals);
-        } else {
-          sqrtPrice = calculateSqrtPriceX96(1 / price, decimals, decimals);
-        }
+        // if (erc20Address[0] == HOGWELL.address) {
+        //   sqrtPrice = calculateSqrtPriceX96(price, decimals, decimals);
+        // } else {
+        //   sqrtPrice = calculateSqrtPriceX96(1 / price, decimals, decimals);
+        // }
+        //This is important if the token already has liquidity or not
+        await NFTPositionManager.createAndInitializePoolIfNecessary(
+          erc20Address[0], // The token addresses need to be sorted
+          erc20Address[1],
+          fee,
+          sqrtPrice.toFixed(0)
+        );
 
+        const realPrice = calculatePriceFromX96(sqrtPrice, decimals, decimals);
+        const factoryAddy = await leveragedV3Manager.uniswapFactory();
+        Factory = await ethers.getContractAt(
+          "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol:IUniswapV3Factory",
+          factoryAddy
+        );
+        
+        const justMadePoolAddy = await Factory.getPool(
+          erc20Address[0],
+          erc20Address[1],
+          fee
+        );
+        //This is the address of the pool we just made
+        HOGEPICPool = await ethers.getContractAt(
+          "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol:IUniswapV3Pool",
+          justMadePoolAddy
+        );
+        //This the starting slot0 of the HOGWELL/EPIC pool I just made
+        const slot0 = await HOGEPICPool.slot0();
+        const tickSpacing = await HOGEPICPool.tickSpacing();
+        let nearestTick = getNearestUsableTick(
+          parseInt(slot0.tick),
+          tickSpacing
+        );
+          //Choose arbitarry tick for testing
+        const lowerTick = nearestTick - tickSpacing * 100;
+        const upperTick = nearestTick + tickSpacing * 100;
+        const token0Amount = new bigDecimal(10**18)
+        const token1Amount = new bigDecimal(10**18)
+        
+          console.log(token0Amount.getValue())
+        const mintParams = {
+          token0: erc20Address[0],
+          token1: erc20Address[1],
+          fee: fee,
+          tickLower: lowerTick,
+          tickUpper: upperTick,
+          amount0Desired: toolbaroken0Amount.getValue(),
+          amount1Desired: token1Amount.getValue(),
+          amount0Min: 0,
+          amount1Min: 0,
+          recipient: deployer.address,
+          deadline: timeStamp + 1000,
+        };
+        await NFTPositionManager.mint(mintParams);
+      
 
       })
       it("user can build a v3 position", async () => {
